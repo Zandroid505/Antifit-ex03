0a110296353770d4ebfa73d9cc0bdf5ac0f4acfd
Zandroid505 <zakariaantifit@hotmail.com>
2021-09-03 08:36:35 -0400

    Initial commit

---
+++ src/main/java/exercise03/base/Solution03.java
@@ -0,0 +1,28 @@
+package exercise03.base;
+
+import java.util.Scanner;
+
+/*
+ * UCF COP3330 Fall 2021 Assignment 1 Solution
+ * Copyright 2021 Zakaria Antifit
+ */
+public class Solution03 {
+/*
+ * print "What is the quote"
+ * 'quote' = user input string
+ * print "Who said it?"
+ * 'author' = user input string
+ * print "'author' says, 'quote'
+ */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is the quote? ");
+        String quote = input.nextLine();
+
+        System.out.print("Who said it? ");
+        String author = input.nextLine();
+
+        System.out.println(author + " says, " + "\"" + quote + "\"");
+    }
+}
